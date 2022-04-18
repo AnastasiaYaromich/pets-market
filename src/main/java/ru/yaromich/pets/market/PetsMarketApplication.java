4d6a8438b3f8d6d8e795866598f88346a8a45075
AnastasiaYaromich <Anastasia281295@gmail.com>
2022-04-18 14:34:30 +0300

    Initial commit

---
+++ src/main/java/ru/yaromich/pets/market/PetsMarketApplication.java
@@ -0,0 +1,13 @@
+package ru.yaromich.pets.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/yaromich/pets/market/PetsMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yaromich.pets.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
